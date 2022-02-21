d073f2abd51212e1f0d626154e714b25171cd4f8
Jamison Coombs <58711684+JCoombs224@users.noreply.github.com>
2022-02-21 11:49:12 -0800

    added quicksort

---
+++ QuickSort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void Partition(int $arr[], int p, int r)
+{
+
+}
+
+void QuickSort(int $arr[], int p, int r)
+{
+    if(p >= r)
+        return;
+    else
+    {
+        
+    }
+}
+
+int main()
+{
+
+}
